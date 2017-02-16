ac64a0c4c53a8e9fa8d95db43644e56b8885a69a
Space <space@471.no>
2017-02-16 21:39:13 +0100

    Fleshed out form page + lots of minor usability/login js tweaks

--- js/gunnerus.js
+++ js/gunnerus.js
@@ -1,12 +1,32 @@
 function DayDiff(CurrentDate, compareDate) {
-			var TYear=CurrentDate.getFullYear();
-			var TDay=new Date(compareDate);
-			TDay.getFullYear(TYear);
-			var DayCount=(TDay-CurrentDate)/(1000*60*60*24);
-			DayCount=Math.round(DayCount); 
-			return(DayCount);
-		}
-		
+	var TYear=CurrentDate.getFullYear();
+	var TDay=new Date(compareDate);
+	TDay.getFullYear(TYear);
+	var DayCount=(TDay-CurrentDate)/(1000*60*60*24);
+	DayCount=Math.round(DayCount); 
+	return(DayCount);
+}
+
+function modalClickHandler(clickEvent, clickedObject) {
+	clickEvent.preventDefault();
+	console.log("toggle modal executed");
+	console.log(clickedObject);
+	var pid = $(clickedObject).data('modal');
+	$.get('/modals/' + pid + '.txt', function(data){
+		var headerRegex = /(?:<header>)(.|[\r\n])*(?:<\/header>)/g;
+		var contentRegex = /(?:<content>)(.|[\r\n])*(?:<\/content>)/g;
+		var headerContent = headerRegex.exec(data)[0];
+		var bodyContent = contentRegex.exec(data)[0];
+		document.querySelector("#txtModal").style.marginTop = document.querySelector(".navbar-header").offsetHeight+"px";
+		$('#txtModal .modal-title').html(headerContent);
+		$('#txtModal .modal-body').html(bodyContent);
+		$('#txtModal').modal('show');
+		$('#txtModal .toggleModal').click(function(ev){modalClickHandler(ev, this);});
+		document.querySelector('#txtModal').focus();
+		$("#txtModal .modal-content").click();
+	});
+}
+
 $(document).ready(function() {
 	
 var calendar = $('#calendar').calendar({
@@ -173,30 +193,9 @@ var calendar = $('#calendar').calendar({
 	});
 	
 	$('#txtModal').modal('hide');
-	$('.toggleModal').click(function(ev){
-		ev.preventDefault();
-		console.log("toggle modal executed");
-		if (!!document.querySelector('body.modal-open')) {
-			$('.modal').modal('hide');
-		} else {
-			console.log(this);
-			var pid = $(this).data('modal');
-			$.get('/modals/' + pid + '.txt', function(data){
-				var headerRegex = /(?:<header>)(.|[\r\n])*(?:<\/header>)/g;
-				var contentRegex = /(?:<content>)(.|[\r\n])*(?:<\/content>)/g;
-				var headerContent = headerRegex.exec(data)[0];
-				var bodyContent = contentRegex.exec(data)[0];
-				document.querySelector("#txtModal").style.marginTop = document.querySelector(".navbar-header").offsetHeight+"px";
-				$('#txtModal .modal-title').html(headerContent);
-				$('#txtModal .modal-body').html(bodyContent);
-				$('#txtModal').modal('show');
-				document.querySelector('#txtModal').focus();
-				$("#txtModal .modal-content").click();
-			});
-		}
-	});
+	$('.toggleModal').click(function(ev){modalClickHandler(ev, this);});
 	
-	$("#navbar.in a").not(".dropdown-toggle").click(function(event) { 
+	$("#navbar.in a").not(".dropdown-toggle").click(function() { 
 		$('.navbar-toggle').click();
 	});
 	
